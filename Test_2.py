84ceab77f962067bd83567b52a6edc7b01089ddf
MBFDM <119826728+MBFDM@users.noreply.github.com>
2026-08-06 01:27:53 +0100

    Update fmt.Println output from 'Hello' to 'Goodbye'

--- Test_2.py
+++ Test_2.py
@@ -1124,8 +1124,8 @@ def page_login():
     with col2:
         st.markdown("""
         <div style="text-align: center; padding: 40px; animation: fadeIn 0.5s;">
-            <h1 style="color: #1e3c72; font-size: 3em; margin-bottom: 10px;">AGC-VIE</h1>
-            <img src="logo_1.jpg" alt="AGC-VIE" width="100px" height="100px">
+            <!--h1 style="color: #1e3c72; font-size: 3em; margin-bottom: 10px;">AGC-VIE</h1-->
+            <img src="logo_1.jpg" width="100px" height="100px">
             <p style="color: #666; font-size: 1.2em; margin-bottom: 30px;">
                 Système de Gestion Technique et Comptable
             </p>
